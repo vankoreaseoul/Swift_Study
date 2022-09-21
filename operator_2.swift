328fabc5a8a7b0272d2ec501f3a35b78bf821851
Seo Heawon <gofltmxj@naver.com>
2022-09-22 02:02:41 +0900

    Create operator_2.swift

---
+++ operator_2.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+// 비교 연산자
+// Comparison Operators
+
+let a = 20
+let b = 30
+
+// Bool
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+
+if a > b {
+     
+} else {
+    
+}
+
+
+
+let c = "a"
+let d = "b"
+
+// Bool
+c == d
+c != d
+c > d
+c < d
+c >= d
+c <= d
+
+if c > d {
+     
+} else {
+    
+}
+
+"\u{61}"    // -> unicode
